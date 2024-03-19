7b2fe96a5acbd1346de63ec70a957b5253be2c96
Harryn Oh <ohharryn0502@gmail.com>
2024-03-19 07:26:52 +0000

    feat: Add examples for setting hyperparameters

--- inference_image.py
+++ inference_image.py
@@ -6,6 +6,11 @@ image = os.path.join("images", file)
 filename = os.path.join("output", file)
 
 predictor = Predictor()
+
+# Set the score and IoU threshold (optional)
+# predictor.set_score_threshold(0.5)
+# predictor.set_iou_threshold(0.95)
+
 instances = predictor.predict(image)
 predictor.save_image(image, filename, instances)
 predictor.save_masks(filename, instances)
