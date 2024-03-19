5f3639ba4a66125315f2fed02a3a38e30e6707e1
Harryn Oh <ohharryn0502@gmail.com>
2024-03-19 18:33:47 +0000

    fix: Unccomment the code

--- inference_image.py
+++ inference_image.py
@@ -1,15 +1,15 @@
 import os
 from utils.predictor import Predictor
 
-file = "sample.jpg"
-image = os.path.join("images", file)
+file = "imagehand8.jpg"
+image = os.path.join("images", "custom", file)
 filename = os.path.join("output", file)
 
 predictor = Predictor()
 
 # Set the score and IoU threshold (optional)
-# predictor.set_score_threshold(0.5)
-# predictor.set_iou_threshold(0.95)
+predictor.set_score_threshold(0.97)
+predictor.set_iou_threshold(0.95)
 
 instances = predictor.predict(image)
 predictor.save_image(image, filename, instances)
--- inference_webcam.py
+++ inference_webcam.py
@@ -3,7 +3,7 @@ from utils.predictor import Predictor
 predictor = Predictor()
 
 # Set the score and IoU threshold (optional)
-# predictor.set_score_threshold(0.5)
-# predictor.set_iou_threshold(0.95)
+predictor.set_score_threshold(0.97)
+predictor.set_iou_threshold(0.95)
 
 predictor.start_webcam()
