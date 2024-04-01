9e4202fe1445ba98290347340dbe601d4c5637b6
LukeJacksonUCL <luke.jackson.21@ucl.ac.uk>
2024-04-01 13:09:45 +0100

    change to two-hand-shape-pose

--- two-hand-shape-pose/demo/demo.py
+++ two-hand-shape-pose/demo/demo.py
@@ -29,11 +29,9 @@ def main():
     test_folder = './test_folder'
     model_path = './snapshot_%d.pth.tar' % int(args.test_epoch)
     model=Model(resnet_version=50,mano_neurons=[512, 512, 512, 512],mano_use_pca=False,cascaded_num=3)
-    #device_run=torch.device('cpu')#'cuda:%d'%(args.gpu))#gpu
-    #ckpt=torch.load("{}".format(model_path))
-    ckpt=torch.load("{}".format(model_path), map_location=device)#new code
+    ckpt=torch.load("{}".format(model_path), map_location=device)
     model.load_state_dict({k.split('.',1)[1]:v for k,v in ckpt['network'].items()})
-    model.to(device)#device_run
+    model.to(device)
     model.eval()
     print('load success')
     INPUT_SIZE=256
@@ -46,8 +44,14 @@ def main():
         M=np.array([[ratio,0,0],[0,ratio,0]],dtype=np.float32)
         img=cv2.warpAffine(img,M,(INPUT_SIZE,INPUT_SIZE),flags=cv2.INTER_LINEAR,borderValue=[0,0,0])
         img=img[:,:,::-1].astype(np.float32)/255
-        input_tensor=torch.tensor(img.copy().transpose(2,0,1),device=device,dtype=torch.float32).unsqueeze(0)#device_run
+        input_tensor=torch.tensor(img.copy().transpose(2,0,1),device=device,dtype=torch.float32).unsqueeze(0)
         out = model({'img':input_tensor}, None, None, 'test')
+        #outputting the mano shape and pose parameters
+        pose = out['pose']
+        shape = out['shape']
+
+        first9pose = pose[0][0][0 : 9] #the first 9 parameters in the pose dict entry
+        first10shape = shape[0][0][0: 10] #the first 10 parameters in the shape dict entry
 
         joint_coord_out = out['joints3d']
         trans = out['trans']
@@ -61,38 +65,25 @@ def main():
             'verts3d':verts3d[:,verts3d.shape[1]//2:,:]-joint_coord_out[:,4+21,None,:],
         }
 
-
         predict_right_length=(right_mano_para['joints3d'][:,4]-right_mano_para['joints3d'][:,0]).norm(dim=1)
         predict_left_length=(left_mano_para['joints3d'][:,4]-left_mano_para['joints3d'][:,0]).norm(dim=1)
         predict_right_verts=right_mano_para['verts3d']/predict_right_length[:,None,None]
         predict_left_verts=left_mano_para['verts3d']/predict_left_length[:,None,None]
-
         predict_left_verts_trans=(predict_left_verts+trans[:,1:].view(-1,1,3))*torch.exp(trans[:,0,None,None])
-
         output_file_name=img_name.split('.')[0]
         
-        with open(os.path.join(test_folder,output_file_name+'_right.obj'),'w') as file_object:
-            for v in predict_right_verts[0]:
-                print("v %f %f %f"%(v[0],v[1],v[2]),file=file_object)
-            for f in right_face+1:
-                print("f %d %d %d"%(f[0],f[1],f[2]),file=file_object)
-
-        with open(os.path.join(test_folder,output_file_name+'_left.obj'),'w') as file_object:
-            for v in predict_left_verts_trans[0]:
-                print("v %f %f %f"%(v[0],v[1],v[2]),file=file_object)
-            for f in left_face+1:
-                print("f %d %d %d"%(f[0],f[1],f[2]),file=file_object)
-
-        with open(os.path.join(test_folder,output_file_name+'_interacting.obj'),'w') as file_object:
-            for v in predict_right_verts[0]:
-                print("v %f %f %f"%(v[0],v[1],v[2]),file=file_object)
-            for v in predict_left_verts_trans[0]:
-                print("v %f %f %f"%(v[0],v[1],v[2]),file=file_object)
-            for f in right_face+1:
-                print("f %d %d %d"%(f[0],f[1],f[2]),file=file_object)
-            for f in left_face+1+predict_right_verts.shape[1]:
-                print("f %d %d %d"%(f[0],f[1],f[2]),file=file_object)
+        with open(os.path.join(test_folder, output_file_name+'_entire_pose_array.txt'), 'w') as file_object:
+            file_object.write("All poses: " + ', '.join(map(str, pose.tolist())) + "\n")
             
+        with open(os.path.join(test_folder, output_file_name+'_entire_shape_array.txt'), 'w') as file_object:
+            file_object.write("All shapes: " + ', '.join(map(str, shape.tolist())) + "\n")
+
+        #outputs some of the data in the needed format
+        #these likely aren't the correct parameters to output, need to find which ones are
+        with open(os.path.join(test_folder, output_file_name+'_formatter_shape_and_pose.txt'), 'w') as file_object:
+            file_object.write("All poses: " + ', '.join(map(str, first9pose.tolist())) + "\n")
+            file_object.write("All betas: " + ', '.join(map(str, first10shape.tolist())) + "\n")
+       
 
 if __name__=='__main__':
     with torch.no_grad():
