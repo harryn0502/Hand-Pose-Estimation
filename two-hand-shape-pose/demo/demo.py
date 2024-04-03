65a087781c5169f5c591fb9d67affdcfa2edf43d
LukeJacksonUCL <luke.jackson.21@ucl.ac.uk>
2024-04-03 11:45:56 +0100

    update to MANO output

--- two-hand-shape-pose/demo/demo.py
+++ two-hand-shape-pose/demo/demo.py
@@ -46,13 +46,8 @@ def main():
         img=img[:,:,::-1].astype(np.float32)/255
         input_tensor=torch.tensor(img.copy().transpose(2,0,1),device=device,dtype=torch.float32).unsqueeze(0)
         out = model({'img':input_tensor}, None, None, 'test')
+
         #outputting the mano shape and pose parameters
-        pose = out['pose']
-        shape = out['shape']
-
-        first9pose = pose[0][0][0 : 9] #the first 9 parameters in the pose dict entry
-        first10shape = shape[0][0][0: 10] #the first 10 parameters in the shape dict entry
-
         joint_coord_out = out['joints3d']
         trans = out['trans']
         verts3d = out['verts3d']
@@ -65,24 +60,21 @@ def main():
             'verts3d':verts3d[:,verts3d.shape[1]//2:,:]-joint_coord_out[:,4+21,None,:],
         }
 
+        ljoints = joint_coord_out[0][0:21]
+        rjoints = joint_coord_out[0][22:42]
+
         predict_right_length=(right_mano_para['joints3d'][:,4]-right_mano_para['joints3d'][:,0]).norm(dim=1)
         predict_left_length=(left_mano_para['joints3d'][:,4]-left_mano_para['joints3d'][:,0]).norm(dim=1)
         predict_right_verts=right_mano_para['verts3d']/predict_right_length[:,None,None]
         predict_left_verts=left_mano_para['verts3d']/predict_left_length[:,None,None]
         predict_left_verts_trans=(predict_left_verts+trans[:,1:].view(-1,1,3))*torch.exp(trans[:,0,None,None])
         output_file_name=img_name.split('.')[0]
-        
-        with open(os.path.join(test_folder, output_file_name+'_entire_pose_array.txt'), 'w') as file_object:
-            file_object.write("All poses: " + ', '.join(map(str, pose.tolist())) + "\n")
-            
-        with open(os.path.join(test_folder, output_file_name+'_entire_shape_array.txt'), 'w') as file_object:
-            file_object.write("All shapes: " + ', '.join(map(str, shape.tolist())) + "\n")
 
-        #outputs some of the data in the needed format
-        #these likely aren't the correct parameters to output, need to find which ones are
-        with open(os.path.join(test_folder, output_file_name+'_formatter_shape_and_pose.txt'), 'w') as file_object:
-            file_object.write("All poses: " + ', '.join(map(str, first9pose.tolist())) + "\n")
-            file_object.write("All betas: " + ', '.join(map(str, first10shape.tolist())) + "\n")
+        with open(os.path.join(test_folder, output_file_name+'_right_joints3D.txt'), 'w') as file_object:
+            file_object.write("Joints3D Right: " + ', '.join(map(str, rjoints.tolist())) + "\n")
+        
+        with open(os.path.join(test_folder, output_file_name+'_left_joints3D.txt'), 'w') as file_object:
+            file_object.write("Joints3D left: " + ', '.join(map(str, ljoints.tolist())) + "\n")
        
 
 if __name__=='__main__':
