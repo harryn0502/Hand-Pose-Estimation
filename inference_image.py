f4a277f68405a6e6299e787d8c858a66eb1f7819
Lucas Marrie <lucas.marrie.21@ucl.ac.uk>
2024-04-08 00:18:10 +0800

    modified ground truth file

--- inference_image.py
+++ inference_image.py
@@ -3,6 +3,15 @@ from src.predictor import Predictor
 from src.mask_image import black_out_region_bulk
 from src.pose_estimator.estimate_pose import estimate_pose
 
+#clear out a directory, or create it if it does not exist
+def clear_directory(directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    else:
+        for filename in os.listdir(directory):
+            for filename in os.listdir(directory):
+                os.remove(os.path.join(directory, filename))
+
 # Set the image and output filename
 images_folder = "images"
 masks_folder = "masks"
@@ -25,31 +34,36 @@ predictor.set_iou_threshold(0.95)
 files = os.listdir(images_folder)
 image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg', '.tiff'))]
 
-if not os.path.isdir(masks_folder):
-    os.makedirs(masks_folder)
+clear_directory(masks_folder)
 
-for image_name in image_files:
+
+for i, image_name in enumerate(image_files):
     image_path = os.path.join(images_folder, image_name)
     output_name = os.path.join(masks_folder, image_name)
 
     instances = predictor.predict(image_path)
     #predictor.save_image(image_path, output_name, instances)
     predictor.save_masks(output_name, instances)
+    print(f"segmentation progress {i+1}/{len(image_files)}", end="\r")
+print()
+print("segmentation completed")
 
 """
 mask images
 """
 #delete masks folder and recreate it
-if not os.path.isdir(masked_images_folder):
-    os.makedirs(masked_images_folder)
+clear_directory(masked_images_folder)
 
 black_out_region_bulk(images_folder, masks_folder, masked_images_folder)
+
+print("masking completed")
 
 """
 Pose estimation
 """
-if not os.path.isdir(output_folder):
-    os.makedirs(output_folder)
+clear_directory(output_folder)
 
 pose_model = "model/snapshot_99.pth.tar"
 estimate_pose(masked_images_folder, output_folder, pose_model)
+
+print("pose estimation completed")
