ce8522b1396983db107f531392a9896458b99f74
Lucas Marrie <lucas.marrie.21@ucl.ac.uk>
2024-04-07 22:49:36 +0800

    added additional information in test output

--- test_segmentation_count.py
+++ test_segmentation_count.py
@@ -26,10 +26,14 @@ mask_files = os.listdir(mask_folder)
 for mask_file in mask_files:
     hand_counts[mask_file.split("_mask")[0]] -= 1
 
+succesful_tests = 0 
 for image, hand_count in sorted(hand_counts.items()):
     if hand_count == 0:
+        succesful_tests += 1
         print(f"test {image}: passed")
     elif hand_count > 0:
         print(f"test {image}: failed not enough masks, missing {hand_count} mask(s)")
     else:
         print(f"test {image}: failed too many masks, additional {-hand_count} mask(s)")
+
+print(f"total tests passed: {succesful_tests}/{len(hand_counts)}")
